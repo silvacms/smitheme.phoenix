729624f6a0122bf1c1aa2cd2d848020999f1fb5e
Antonin Amand <antonin.amand@gmail.com>
2011-01-21 16:17:39 +0100

    add view to define preview frame top height

--- src/smitheme/phoenix/skin.py
+++ src/smitheme/phoenix/skin.py
@@ -1,5 +1,8 @@
+from five import grok
 from silva.core.smi.interfaces import ISMILayer
 from silva.core import conf as silvaconf
+from silva.core.smi import preview
+
 
 class ISMIPhoenixLayer(ISMILayer):
     pass
@@ -8,5 +11,10 @@ class ISMIPhoenixLayer(ISMILayer):
 class ISMIPhoenixSkin(ISMIPhoenixLayer):
     silvaconf.resource('reset.css')
     silvaconf.resource('phoenix.css')
+
+
+class PreviewFrameset(preview.PreviewFrameset):
+    grok.layer(ISMIPhoenixLayer)
+    rows = '101px,*'
 
 
